d51bc121a3319ae02971c6b15ab8de7521f797f9
NerlyTon <nerlyton@gmail.com>
2020-11-13 16:45:41 -0600

    got the scores to show up on the page

--- cube-frontend/index.js
+++ cube-frontend/index.js
@@ -4,14 +4,19 @@ function start() {
     function getGameScores() {
         fetch(`http://localhost:3000/users`)
             .then(res => res.json())
-            .then(users => 
-            users.forEach(user => {
+            .then(function (users) {
+                scorelist.innerHTML += "<ul>"
+            users.forEach((user) => {
                 const newUser = new User(user.username, user.games)
                 console.log(newUser)
-            })
-            )}
-
+                scorelist.innerHTML += newUser.displayScoresHTML();
+            })})
+            scorelist.innerHTML += "</ul>"
+            
+    }
+            
     getGameScores();
+    
 }
 
 
--- cube-frontend/user.js
+++ cube-frontend/user.js
@@ -3,4 +3,12 @@ class User {
         this.username = username
         this.games = games
     }
+
+    displayScoresHTML() {
+        return `<div>
+        <h3> ${this.username} </h3>
+        <li>${this.games.map((game) => `<p>Score:${game.score}</p>`).join("")}</li>
+        
+        </div>`
+    }
 }
