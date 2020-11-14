a6ad78bf8eeb03a38d67a9ca1f0b01a107c429d0
NerlyTon <nerlyton@gmail.com>
2020-11-14 09:50:27 -0600

    destroy action in the games controller set up

--- cube-game-backend/app/controllers/games_controller.rb
+++ cube-game-backend/app/controllers/games_controller.rb
@@ -4,4 +4,11 @@ class GamesController < ApplicationController
         @games = Game.all
         render json: @games
     end
+
+    def destroy
+        @game = Game.find(params[:id])
+        @game.destroy
+        render json: @game
+    end
+
 end
