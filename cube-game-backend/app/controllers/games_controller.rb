78abface8772c2f8821a491ab66ebf9128f7f4de
NerlyTon <nerlyton@gmail.com>
2020-11-14 11:43:43 -0600

    added destroy action in games controller

--- cube-game-backend/app/controllers/games_controller.rb
+++ cube-game-backend/app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
 
     def index
         @games = Game.all
-        render json: @games
+        render json: @games, include: :user
     end
 
     def destroy
