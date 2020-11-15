88625d9145964edb5b9a71167af086ddf115b570
NerlyTon <nerlyton@gmail.com>
2020-11-14 22:32:06 -0600

    canvas set up

--- cube-frontend/game.js
+++ cube-frontend/game.js
@@ -8,10 +8,42 @@
 //     // ctx.stroke()
 // 
 
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 // console.log(middle)
 canvas.width = 871;
 canvas.height = 600;
-ctx.fillRect(50, 100, 300, 100);
-ctx.fill('red')
+ctx.fillStyle = "#FFFFFF"
+ctx.fillRect(380, 500, 100, 100);
+
+
+// window.addEventListener("keydown", function(e) {
+//     e.
+// })
+
+// let x = 200;
+// let dx = 1;
+// let y = 200;
+// let dy = 1;
+
+// function animate() {
+//     requestAnimationFrame(animate);
+//     ctx.clearRect(0,0, innerWidth, innerHeight)
+//     ctx.fillStyle = "#FFFFFF"
+//     ctx.fillRect(x, y, 100, 100);
+
+//     if(x > 750 || x < 200) {
+//         dx = -dx;
+
+//     }
+
+//     if(y > 500 || y < 70) {
+//         dy = -dy;
+
+//     }
+//     x += dx;
+//     y += dy;
+// }
+
+// animate();
+
--- cube-frontend/user.js
+++ cube-frontend/user.js
@@ -3,15 +3,12 @@ class User {
         this.username = username
         this.games = games
     }
-
-    // getUsername(form) {
-    //     return this.username = username.value
-    // }
-
+    
+    
     displayScoresHTML() {
         return `<h3> ${this.username} </h3>
         ${this.games.map((game) => `<div> <p id=${game.id}>Score:${game.score} <button class="delete">Delete</button></p>`).join("")}
-        
         </div>`
     }
+
 }
