6dca2351a0654e3a8e27d5800dfb287fed1dba6e
NerlyTon <nerlyton@gmail.com>
2020-11-18 18:07:47 -0600

    got the logic for login and logout started

--- cube-frontend/user.js
+++ cube-frontend/user.js
@@ -1,10 +1,14 @@
 class User {
-    constructor(username, games) {
+    constructor(id, username, games) {
+        this.id = id
         this.username = username
         this.games = games
+        User.all.push(this)
     }
+
     
     
+    static all = []
     displayScoresHTML() {
         return `<h3> ${this.username} </h3>
         ${this.games.map((game) => `<div> <p id=${game.id}>Score:${game.score} <button class="delete">Delete</button></p>`).join("")}
@@ -12,3 +16,27 @@ class User {
     }
 
 }
+
+const columnLeft = document.querySelector(".left")
+const scorelist = document.getElementById("scoreList")
+
+
+function hideForm() {
+    userForm.style.display = "none"
+    columnLeft.innerHTML +=  '<div id="logoutDiv"><h2>LogOut</h2><p><button class="logoutBtn">LogOut</button></p></div>'
+    showForm()
+    // debugger
+}
+
+
+function showForm() {
+    const logout = document.querySelector(".logoutBtn")
+    logout.addEventListener("click", (e) => {
+        e.preventDefault()
+        // debugger
+    userForm.style.display = "block"
+    document.querySelector("#logoutDiv").style.display = "none"
+    scorelist.innerHTML = ""
+
+    }
+)}
