023c73687f8bbba59f7b15c797cb23d4b58c3b1b
NerlyTon <nerlyton@gmail.com>
2020-11-18 18:06:59 -0600

    added games create action for the game post fetch

--- cube-game-backend/app/controllers/games_controller.rb
+++ cube-game-backend/app/controllers/games_controller.rb
@@ -5,10 +5,22 @@ class GamesController < ApplicationController
         render json: @games, include: :user
     end
 
+    def create
+        @user = User.find(params[:user_id])
+        @game = Game.create(score: params[:score], user_id: params[:user_id])
+        render json: @game, include: :user
+        # @user = User.new(user_params)
+        # if @user.save
+        #     render json: @user
+        # else
+        #     render json:{ errors: @user.errors.full_messages} 
+        # end
+    end
+
     def destroy
         @game = Game.find(params[:id])
         @game.destroy
-        render json: @game
+        render json: @game, include: :user
     end
 
 end
